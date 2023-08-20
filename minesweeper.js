3f2c97dab248e947f210dd38489defbdb8ea6364
VMJ <viktor.mihaly.jozsa@gmail.com>
2023-08-20 09:43:20 +0200

    kepkocka pozicionalas canvason

--- minesweeper.js
+++ minesweeper.js
@@ -1,4 +1,10 @@
-var image = document.getElementById('hidden');
-var canvas = document.getElementById('myCanvas');
-var ctx = canvas.getContext('2d');
-ctx.drawImage(image, 0, 0); // A kép bal felső sarka (0,0) koordinátára kerül.
+const image = document.getElementById('hidden');
+const canvas = document.getElementById('myCanvas');
+const c = canvas.getContext('2d');
+
+const size = 50;
+let x = 100;
+let y = 50;
+
+c.drawImage(image, x, y, size, size); // A kép bal felső sarka (x,y) koordinátára kerül
+c.drawImage(image, x + size, y + size, size, size);
