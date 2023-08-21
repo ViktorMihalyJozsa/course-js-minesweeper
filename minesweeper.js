16ecb4737e0831887c3c72215b71db274e8f61e0
VMJ <viktor.mihaly.jozsa@gmail.com>
2023-08-21 09:45:26 +0200

    kirajzoljuk a palyat

--- minesweeper.js
+++ minesweeper.js
@@ -4,11 +4,19 @@ const c = canvas.getContext('2d'); // A canvas context-je, amivel rajzolunk.
 
 const size = 50; // A képek mérete.
 const columns = canvas.width / size; // A képek száma.
+const rows = canvas.height / size; // A képek száma.
 
-for (let i = 0; i < columns; i++) {
-    drawImage(i * size, 0);
+drawMap(); // Kirajzoljuk a pályát.
+
+function drawMap() {
+    for (let i = 0; i < columns; i++) {
+        for (let j = 0; j < rows; j++) {
+
+            drawImage(i * size, j * size); // Kirajzoljuk a képet.
+        }
+    }
 }
 
 function drawImage(x, y) {
-    c.drawImage(image, x, y, size, size);
+    c.drawImage(image, x, y, size, size); // Kirajzoljuk a képet.
 }
