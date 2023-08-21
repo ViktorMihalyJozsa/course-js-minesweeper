fedb9de85ef64c479e8ff3db6e181296b0789e49
VMJ <viktor.mihaly.jozsa@gmail.com>
2023-08-21 09:22:33 +0200

    elso sor letrehozasa

--- minesweeper.js
+++ minesweeper.js
@@ -3,10 +3,11 @@ const canvas = document.getElementById('myCanvas'); // A canvas, amire rajzolunk
 const c = canvas.getContext('2d'); // A canvas context-je, amivel rajzolunk.
 
 const size = 50; // A képek mérete.
+const columns = canvas.width / size; // A képek száma.
 
-drawImage(0, 0); // Kirajzoljuk a képet a kezdő koordinátákon.
-drawImage(100, 100);
-drawImage(200, 200);
+for (let i = 0; i < columns; i++) {
+    drawImage(i * size, 0);
+}
 
 function drawImage(x, y) {
     c.drawImage(image, x, y, size, size);
